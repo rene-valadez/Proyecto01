2c5619fb57a0d3d8ba89d465adab4a435a28eaeb
rene-valadez <rene.valadez@softtek.com>
2017-03-29 16:51:29 -0600

    primera commit

---
+++ com/softtek/javasample/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.softtek.javasample;
+/*
+   AUTHOR: RENE VALADEZ
+   
+   OBJ: Exercise for GIT synchronization
+*/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        HelloWorld sample = new HelloWorld();
+        sample.Greetings();
+	}
+
+	void Greetings() {
+		System.out.println("Hellow Git!");
+	}
+	
+}
